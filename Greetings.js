a046eaf33a827d142dee55deb23a5b950ee8aabc
Builtforwoods <rodolfomlphx@gmail.com>
2019-11-21 18:56:43 -0700

    Made the GREETINGS lesson work

--- Greetings.js
+++ Greetings.js
@@ -4,34 +4,50 @@ $(document).ready(function () {
         $("#start").remove();
 
     });
+    $("#necks").click(function () {
+        changeImage();
 
+    });
+    $("#beck").click(function () {
+        backImage();
 
-});
+    });
+    var images = [];
 
-var images = [];
+    images[0] = 'greetingsquestionsset/questionTwo.png';
+    images[1] = 'greetingsquestionsset/questionThree.png';
+    images[2] = 'greetingsquestionsset/questionFour.png';
+    images[3] = 'greetingsquestionsset/questionFive.png';
+    images[4] = 'greetingsquestionsset/questionSix.png';
+    images[5] = 'greetingsquestionsset/questionSeven.png';
+    images[6] = 'greetingsquestionsset/questionEight.png';
+    images[7] = 'greetingsquestionsset/questionNine.png';
+    images[8] = 'greetingsquestionsset/questionTen.png';
+    images[9] = 'greetingsquestionsset/questionEleven.png';
 
-images[0] = 'greetingsquestionsset/questionTwo.png';
-images[1] = 'greetingsquestionsset/questionThree.png';
-images[2] = 'greetingsquestionsset/questionFour.png';
-images[3] = 'greetingsquestionsset/questionFive.png';
-images[4] = 'greetingsquestionsset/questionSix.png';
-images[5] = 'greetingsquestionsset/questionSeven.png';
-images[6] = 'greetingsquestionsset/questionEight.png';
-images[7] = 'greetingsquestionsset/questionNine.png';
-images[8] = 'greetingsquestionsset/questionTen.png';
-images[9] = 'greetingsquestionsset/questionEleven.png';
+    var i;
+    function changeImage() {
+        $('#screenImage').attr('src', images[i]);
 
-function changeImage() {
-    document.screenImage.src = image[i];
+        if (i < images.length - 1) {
+            i++;
+        } else {
+            i = 0;
+        }
 
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
     }
 
-}
+    function backImage() {
+        $("#screenImage").attr('src', images[i]);
+        if (i < images.length - 1) {
+            i--;
+        } else {
+            i = 0;
+        }
+        console.log(images[i]);
+    }
+});
 
-becks.onclick = function () {
-    changeImage();
-}
+
+
+
